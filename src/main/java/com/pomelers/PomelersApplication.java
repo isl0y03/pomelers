3fd3ee8350a325d461654dc2110f6f829375b092
isl0y03 <isl0y03@gmail.com>
2020-01-13 11:56:57 +0900

    Initial commit
    Create new spring starter project.

---
+++ src/main/java/com/pomelers/PomelersApplication.java
@@ -0,0 +1,13 @@
+package com.pomelers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PomelersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PomelersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pomelers/PomelersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pomelers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PomelersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
